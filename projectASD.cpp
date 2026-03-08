249325cc26aa6e6252ecc36a05dcf2a93dcfd1e2
Tegar Ludwinio <tegarmahardhika66@gmail.com>
2026-03-08 20:27:14 +0700

    Add files via upload

--- projectASD.cpp
+++ projectASD.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<conio.h>
+#include<iomanip>
 using namespace std;
 
 /*deklarasi prosedur yang digunakan*/
@@ -23,19 +24,25 @@ void HitungDapatnyaBunga()
 
 {
     /*deklarasi*/
-    float waktu, bunga, modal, dapatnya, total;
+    double waktu, bunga, modal, dapatnya, total;
+
     /*Algoritma*/
-  
     cout<<"bunga (%): ";
     cin>>bunga;
+
     cout<<"modal (Rp): ";
     cin>>modal;  
+
     cout<<"waktu (bulan)?: ";
     cin>>waktu;
+
     dapatnya = (bunga / 100) * modal * waktu;
     total = modal + dapatnya;
-    cout<<"dapatnya bunga: "<<dapatnya<<endl;
-    cout<<"total: "<<total<<endl;
+
+    cout<<fixed<<setprecision(2);
+    cout<<"dapatnya bunga: Rp "<<dapatnya<<endl;
+
+    cout<<"total: Rp "<<total<<endl;
     getch();
     
 }
